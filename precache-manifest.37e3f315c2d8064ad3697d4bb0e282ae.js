0037fa6d759f0c16ae17145e14d12ffcce7e8b09
Neil Merton <32037777+scp-nm@users.noreply.github.com>
2019-07-03 20:14:07 +0100

    Initial commit

---
+++ precache-manifest.37e3f315c2d8064ad3697d4bb0e282ae.js
@@ -0,0 +1,20 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/static/css/main.85871852ff883f14a5b2.css"
+  },
+  {
+    "revision": "c4a195f521a9c338c6e2c13ab95c1c46",
+    "url": "index.html"
+  },
+  {
+    "revision": "28abb7b79ee2b01c26c2277a600c61f8",
+    "url": "static/images/alien.png"
+  },
+  {
+    "revision": "48e5f38a2b23078e15d22e8832904747",
+    "url": "static/images/alien.svg"
+  },
+  {
+    "url": "static/js/main.3cb81d582aa81aaa38fe.js"
+  }
+]);
